0ee74db1e8d3defb7c1d846abe46e5e67c6cdea3
ruben <rubengarcia0515@gmail.com>
2025-03-22 13:41:21 -0500

    add annotation to avoid error when not running dev env

--- src/main/java/com/example/demo/util/Ngrok/NgrokController.java
+++ src/main/java/com/example/demo/util/Ngrok/NgrokController.java
@@ -1,9 +1,11 @@
 package com.example.demo.util.Ngrok;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("dev")
 @RestController
 @RequestMapping("/api")
 public class NgrokController {
